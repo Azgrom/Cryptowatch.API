bd25eb0ec3dd7a5ca1f868b832c5e31845226910
Rafael Lúcio <rafaellucio@pm.me>
2024-09-29 19:05:11 -0300

    feat: server time and server status responses definitions

--- CryptoWatch.REST.API/CryptoWatchApi.cs
+++ CryptoWatch.REST.API/CryptoWatchApi.cs
@@ -15,3 +15,43 @@ public class CryptoWatchRestApi
     public MarketsApi Markets => new(_httpClient);
     public ExchangesApi Exchanges => new(_httpClient);
 }
+
+public class ServerTimeResponse
+{
+    [JsonPropertyName("error")]
+    public Error[] error { get; set; }
+
+    [JsonPropertyName("result")]
+    public ServerTime result { get; set; }
+}
+
+public class ServerTime
+{
+    [JsonPropertyName("unixtime")]
+    public int unixtime { get; set; }
+
+    [JsonPropertyName("rfc1123")]
+    public string rfc1123 { get; set; }
+}
+
+public class SystemStatusResponse
+{
+    [JsonPropertyName("error")]
+    public Error[] error { get; set; }
+
+    [JsonPropertyName("result")]
+    public SystemStatus result { get; set; }
+}
+
+public class SystemStatus
+{
+    [JsonPropertyName("status")]
+    public string status { get; set; }
+
+    [JsonPropertyName("timestamp")]
+    public string timestamp { get; set; }
+}
+
+public class Error
+{
+}
