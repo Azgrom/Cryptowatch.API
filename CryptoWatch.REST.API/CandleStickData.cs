10cc26d909f53c9ab4fd89f7ab69728312fc9d67
Rafael Lúcio <rafaellucio@pm.me>
2024-10-03 18:55:57 -0300

    feat: candlestick data

---
+++ CryptoWatch.REST.API/CandleStickData.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Text.Json.Serialization;
+
+namespace CryptoWatch.REST.API;
+
+public readonly struct CandleStickData : IDictionary<string, double[][]>
+{
+    private readonly IDictionary<string, double[][]> _dictionaryImplementation;
+
+    [JsonConstructor]
+    public CandleStickData(IDictionary<string, double[][]> dictionaryImplementation, int last)
+    {
+        _dictionaryImplementation = dictionaryImplementation;
+        Last                      = last;
+    }
+
+    [JsonPropertyName("last")]
+    public int Last { get; }
+
+    [JsonIgnore] public double[][] this[string key]
+    {
+        get => _dictionaryImplementation[key];
+        set => _dictionaryImplementation[key] = value;
+    }
+
+    [JsonIgnore] public int                     Count      => _dictionaryImplementation.Count;
+    [JsonIgnore] public bool                    IsReadOnly => _dictionaryImplementation.IsReadOnly;
+    [JsonIgnore] public ICollection<string>     Keys       => _dictionaryImplementation.Keys;
+    [JsonIgnore] public ICollection<double[][]> Values     => _dictionaryImplementation.Values;
+
+    public IEnumerator<KeyValuePair<string, double[][]>> GetEnumerator() => _dictionaryImplementation.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_dictionaryImplementation).GetEnumerator();
+
+    public void Add(KeyValuePair<string, double[][]> item) { _dictionaryImplementation.Add(item); }
+
+    public void Clear() { _dictionaryImplementation.Clear(); }
+
+    public bool Contains(KeyValuePair<string, double[][]> item) => _dictionaryImplementation.Contains(item);
+
+    public void CopyTo(KeyValuePair<string, double[][]>[] array, int arrayIndex) =>
+        _dictionaryImplementation.CopyTo(array, arrayIndex);
+
+    public bool Remove(KeyValuePair<string, double[][]> item) => _dictionaryImplementation.Remove(item);
+
+    public void Add(string key, double[][] value) { _dictionaryImplementation.Add(key, value); }
+
+    public bool ContainsKey(string key) => _dictionaryImplementation.ContainsKey(key);
+
+    public bool Remove(string key) => _dictionaryImplementation.Remove(key);
+
+    public bool TryGetValue(string key, out double[][] value) => _dictionaryImplementation.TryGetValue(key, out value);
+}
+
+public readonly struct CandleStickResponse
+{
+    [JsonPropertyName("error")]
+    public Error[] Error { get; }
+
+    [JsonPropertyName("result")]
+    public CandleStickData CandleStickData { get; }
+}
